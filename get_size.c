8f07c228bc5bc7c163bdb9a543ecd0ad64d80539
Youssef <myyoussef2003@hotmail.com>
2023-09-22 10:10:38 -0700

    e

--- get_size.c
+++ get_size.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -6,24 +5,22 @@
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
- * Return: Precision.
+ * Return: Size.
  */
 int get_size(const char *format, int *i)
 {
-	int curr_i = *i + 1;
-	int size = 0;
+    int curr_i = *i + 1;
+    int size = 0;
 
-	if (format[curr_i] == 'l')
-		size = S_LONG;
-	else if (format[curr_i] == 'h')
-		size = S_SHORT;
+    if (format[curr_i] == 'l')
+        size = S_LONG;
+    else if (format[curr_i] == 'h')
+        size = S_SHORT;
+    else
+        *i = curr_i - 1;
 
-	if (size == 0)
-		*i = curr_i - 1;
-	else
-		*i = curr_i;
+    *i = curr_i;
 
-	return (size);
+    return size;
 }
-
 
--- get_width.c
+++ get_width.c
@@ -10,31 +10,31 @@
  */
 int get_width(const char *format, int *i, va_list list)
 {
-	int curr_i;
-	int width = 0;
+    int curr_i = *i + 1;
+    int width = 0;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		if (is_digit(format[curr_i]))
-		{
-			width *= 10;
-			width += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
-			curr_i++;
-			width = va_arg(list, int);
-			break;
-		}
-		else
-			break;
-	}
+    while (format[curr_i] != '\0')
+    {
+        if (is_digit(format[curr_i]))
+        {
+            width *= 10;
+            width += format[curr_i] - '0';
+            curr_i++;
+        }
+        else if (format[curr_i] == '*')
+        {
+            width = va_arg(list, int);
+            curr_i++;
+            break;
+        }
+        else
+        {
+            break;
+        }
+    }
 
-	*i = curr_i - 1;
+    *i = curr_i - 1;
 
-	return (width);
+    return width;
 }
-
-
-
 
